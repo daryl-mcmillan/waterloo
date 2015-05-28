5c31632324cdf15480143029764f407db4f10ebf
Daryl McMillan <darylm@gmail.com>
2015-05-28 01:12:39 -0400

    maps

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# waterloo
+Waterloo
+========
+
+This is a map of Waterloo, using google maps with overlays from OpenStreetMap.
+
+The OpenStreetMap data was extracted as kml using Query2Map
+http://wiki.openstreetmap.org/wiki/Query-to-map
+
+The KML data was converted to GeoJSON using http://geojson.io/
---
+++ main.js
@@ -0,0 +1,89 @@
+var mapNode = document.querySelector("#map");
+
+var geocoder = new google.maps.Geocoder();
+
+var center = new google.maps.LatLng(43.4583963,-80.5465509);
+var searchBounds = new google.maps.LatLngBounds(
+	new google.maps.LatLng(center.lat()-0.4,center.lng()-0.4),
+	new google.maps.LatLng(center.lat()+0.4,center.lng()+0.4)
+);
+var mapOptions = { zoom: 12, center: center };
+var map = new google.maps.Map(mapNode, mapOptions);
+var marker = new google.maps.Marker({
+	map: map,
+	position: center
+});
+
+layers = {};
+
+function addLayer( name, src, color ) {
+  var layer = new google.maps.Data();
+  layer.loadGeoJson(src);
+  layer.setStyle({
+    strokeColor : color,
+    icon : {
+      path: google.maps.SymbolPath.CIRCLE,
+      strokeColor: color,
+      fillColor: color,
+      fillOpacity: 1.0,
+      scale: 2
+    }
+  });
+  layer.setMap(map);
+  var label = document.createElement("label");
+  label.appendChild( document.createTextNode( name + ":" ) );
+  var toggle = document.createElement("input");
+  toggle.type = "checkbox";
+  toggle.checked = true;
+  toggle.onchange = function() {
+    if( toggle.checked ) {
+      layer.setMap(map);
+    } else {
+      layer.setMap(null);
+    }
+  };
+  label.appendChild(toggle);
+  var parent = document.getElementById("toggles");
+  parent.appendChild( label );
+  layers[name] = layer;
+  google.maps.event.addListener(layer, 'mouseover', function( e ) {
+    var title = e.feature.getProperty("name") || "??";
+    $("#info").text(title);
+  });
+  google.maps.event.addListener(layer, 'mouseout', function( x ) {
+    $("#info").text("");
+  });
+}
+
+addLayer( 'power', 'data/power.json', 'red' );
+addLayer( 'trains', 'data/trains.json', 'blue' );
+addLayer( 'schools', 'data/schools.json', 'green' );
+
+function showAddress( address ) {
+  console.log( { address: address } );
+  var search = { address: address, bounds: searchBounds };
+	geocoder.geocode( search, function( results, status ) {
+		if( status != google.maps.GeocoderStatus.OK ) {
+			console.log( "geocode error", status );
+			return;
+		}
+		var coords = results[0].geometry.location;
+		var title = results[0].formatted_address;
+		console.log( results[0] );
+		marker.setPosition(coords);
+		marker.setTitle(title);
+		map.setCenter(coords);
+	});
+}
+
+var addr = document.getElementById("addr");
+var presses = 0;
+addr.onkeypress = function() {
+  presses ++;
+  setTimeout( function() {
+    presses--;
+    if( presses === 0 ) {
+      showAddress( addr.value );
+    }
+  }, 500);
+};
